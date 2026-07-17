95c9ce972cfce929778975a9ae097a41564ba7a1
Diegue83 <135059873+Diegue83@users.noreply.github.com>
2026-07-17 14:30:07 -0600

    Revert "finalizacion modulo 3.1"
    This reverts commit 5bdf908cf12debf665baa23f3716c86775f2d808.

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "mx.utng.tv"
-        minSdk = 30
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
